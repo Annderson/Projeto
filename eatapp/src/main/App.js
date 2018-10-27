306e82b77951af2c003e9a82f77709dc067b86b8
Anderson Lima <anderson.rl.tr@gmail.com>
2018-10-27 16:03:30 -0300

    first

---
+++ eatapp/src/main/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Menu from '../components/menu/Menu';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Menu/>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+
+          </p>
+      </div>
+    );
+  }
+}
+export default App;
+
+//#4f3631#533darkslategrayhsl(9,23,25)rgb(79,54,49)
+
+//#55a068#5a7mediumseagreenhsl(135,30,48)rgb(85,160,104)
+
