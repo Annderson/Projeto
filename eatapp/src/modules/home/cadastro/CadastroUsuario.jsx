02b91e8ad197780af072783a2ef1c3c17f23ad1e
Anderson Lima <anderson.rl.tr@gmail.com>
2018-11-06 00:45:06 -0200

    cadastro

--- eatapp/src/modules/home/cadastro/CadastroUsuario.jsx
+++ eatapp/src/modules/home/cadastro/CadastroUsuario.jsx
@@ -1,20 +1,38 @@
 import React, { Component } from 'react';
 
+import './CadastroUsuario.css';
+
 export default class CadastroUsuario extends Component {
     render() {
         return(
             <div className="form-cadastro">
                 <form action="" className="panel-cadastro">
-                    <h4 className="text-center">Sign in</h4>
-                    <div className="form-group group-inputs" style={{display:'flex'}}>
-                        <i className="fa fa-user"></i>
-                        <input type="text" name="email" className="inputs-login"/>
-                    </div>
-                    <div className="form-group group-inputs" style={{display:'flex'}}>
-                        <i class="fa fa-key"></i>
-                        <input type="password" name="email" className="inputs-login"/>
-                    </div>
-                    <button className="btn btn-success btn-block btn-md">Entrar</button>
+                    <h4 className="text-center">Cadastro usuário</h4>
+                    <div className="row">
+                        <div className="col-lg-6 col-md-6 col-sm-6">
+                            <div className="form-group group-inputs">
+                                <input type="text" name="name" className="inputs-login"/>
+                            </div>
+                            <div className="form-group group-inputs">
+                                <input type="text" name="name" className="inputs-login"/>
+                            </div>
+                            <div className="form-group group-inputs">
+                                <input type="text" name="name" className="inputs-login"/>
+                            </div>
+                        </div>
+                        <div className="col-lg-6 col-md-6 col-sm-6">
+                            <div className="form-group group-inputs">
+                                <input type="text" name="name" className="inputs-login"/>
+                            </div>
+                            <div className="form-group group-inputs">
+                                <input type="text" name="name" className="inputs-login"/>
+                            </div>
+                            <div className="form-group group-inputs">
+                                <input type="text" name="name" className="inputs-login"/>
+                            </div>
+                        </div>
+                    </div>    
+                    <button className="btn btn-success text-right btn-md">Salvar</button>
                 </form>
             </div>
         )
